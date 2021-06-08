4c8d8eb31a97fe3923b6df4acc6ccbc53d0a3eb9
6erik <estanton@csumb.edu>
2021-06-07 17:32:43 -0700

    Added printFib method to Fibonacci

--- java_work/misc/Fibonacci.java
+++ java_work/misc/Fibonacci.java
@@ -3,19 +3,37 @@
 
 
 public class Fibonacci {
-    public static int getFib(int i) {
+    public static int getFib(int num) {
         int result = 0;
-        if (i <= 2) {
+        if (num <= 2) {
             result = 1;
         }
-        else if (i > 2) {
-            result = getFib(i-2) + getFib(i-1);
+        else if (num > 2) {
+            result = getFib(num-2) + getFib(num-1);
         }
 
         return result;
     }
 
+    public static void printFib(int num) {
+        int[] nums = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            if (i <= 1) {
+                nums[i] = 1;
+            } else {
+                nums[i] = nums[i-1] + nums[i-2];
+            }
+        }
+
+        for (int i = 0; i < num; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
     public static void main(String args[]) {
         System.out.println(getFib(7));
+
+        printFib(7);
     }
 }
